be46fb9847e9108ef3afe1d39e306d63e69a51a9
Jan Piskvor Martinec <github@piskvor.org>
2019-03-21 11:03:01 +0100

    add further forwards

--- autossh-loop.sh
+++ autossh-loop.sh
@@ -6,21 +6,29 @@ set -euxo pipefail
 SSH_REMOTE_HOST=$1
 shift
 
-HOSTNAME_TO_INT=$(sed 's/[^0-9]//g' < /etc/ssh/ssh_host_rsa_key.pub | cut -b1-4)
+HOSTNAME_TO_INT=$(sed 's/[^0-9]//g' < /etc/ssh/ssh_host_rsa_key.pub)
+HOSTNAME_TO_INT_3=$(echo ${HOSTNAME_TO_INT} | cut -b1-3)
+HOSTNAME_TO_INT_4=$(echo ${HOSTNAME_TO_INT} | cut -b1-4)
 LOCAL_LOCAL_ADDR="127.$(hostname -I | cut -f1 "-d " | sed 's/^[0-9]\+.//')"
 
-for i in 1 2 3 4 5 ; do
-    REMOTE_PORT="${i}${HOSTNAME_TO_INT}"
-    REMOTE_PORT_FORWARD="-R ${LOCAL_LOCAL_ADDR}:${REMOTE_PORT}:localhost:22"
+for i in 2 3 4 5 ; do
+    REMOTE_PORT_4="${i}${HOSTNAME_TO_INT_3}"
+    REMOTE_PORT_5="${i}${HOSTNAME_TO_INT_4}"
+    REMOTE_PORT_FORWARD="-R ${LOCAL_LOCAL_ADDR}:${REMOTE_PORT_4}:localhost:22"
+    REMOTE_PORT_FORWARD="${REMOTE_PORT_FORWARD} -R ${LOCAL_LOCAL_ADDR}:${REMOTE_PORT_5}:localhost:222"
+
+    LOCAL_PORT_FORWARD="-L 13124:localhost:3128"
+    DYNAMIC_PORT_FORWARD="-D 23124"
 
     /usr/bin/autossh -NT \
-        -o ServerAliveInterval=30 \
-        -o ServerAliveCountMax=2 \
+        -o ServerAliveInterval=20 \
+        -o ServerAliveCountMax=3 \
         -o StrictHostKeyChecking=yes \
         -o IdentitiesOnly=yes \
         -o ExitOnForwardFailure=yes \
         -o BatchMode=yes \
         -o LogLevel=ERROR \
-        ${REMOTE_PORT_FORWARD} ${SSH_REMOTE_HOST}
+        ${REMOTE_PORT_FORWARD} ${LOCAL_PORT_FORWARD} ${DYNAMIC_PORT_FORWARD} \
+        ${SSH_REMOTE_HOST} "$@"
     sleep 10
 done
