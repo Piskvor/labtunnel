2b0638f873d896a7502a031372356aae9b736075
Jan Piskvor Martinec <github@piskvor.org>
2019-03-21 09:56:23 +0100

    formatting

--- autossh-loop.sh
+++ autossh-loop.sh
@@ -2,14 +2,25 @@
 
 set -euxo pipefail
 
+# if we are to fail, fail quickly
+SSH_REMOTE_HOST=$1
+shift
+
 HOSTNAME_TO_INT=$(sed 's/[^0-9]//g' < /etc/ssh/ssh_host_rsa_key.pub | cut -b1-4)
 LOCAL_LOCAL_ADDR="127.$(hostname -I | cut -f1 "-d " | sed 's/^[0-9]\+.//')"
-REMOTE_HOST=$1
 
 for i in 1 2 3 4 5 ; do
     REMOTE_PORT="${i}${HOSTNAME_TO_INT}"
-    REMOTE_FORWARD="-R ${LOCAL_LOCAL_ADDR}:${REMOTE_PORT}:localhost:22"
+    REMOTE_PORT_FORWARD="-R ${LOCAL_LOCAL_ADDR}:${REMOTE_PORT}:localhost:22"
 
-    /usr/bin/autossh -NT -o ServerAliveInterval=30 -o ServerAliveCountMax=2 -o StrictHostKeyChecking=yes -o IdentitiesOnly=yes -o ExitOnForwardFailure=yes -o BatchMode=yes -o LogLevel=ERROR ${REMOTE_FORWARD} $REMOTE_HOST
+    /usr/bin/autossh -NT \
+        -o ServerAliveInterval=30 \
+        -o ServerAliveCountMax=2 \
+        -o StrictHostKeyChecking=yes \
+        -o IdentitiesOnly=yes \
+        -o ExitOnForwardFailure=yes \
+        -o BatchMode=yes \
+        -o LogLevel=ERROR \
+        ${REMOTE_PORT_FORWARD} ${SSH_REMOTE_HOST}
     sleep 10
 done
