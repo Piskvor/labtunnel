96c705c65914ef39b4c68ddfbcb413edff0e9c08
Jan Piskvor Martinec <github@piskvor.org>
2019-04-26 14:36:58 +0200

    update labtunnel

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 USER_SYSTEMD_DIR=$HOME/.config/systemd/user/
 mkdir -p $USER_SYSTEMD_DIR # create systemd user dir
-cp ./labtunnel@.service $USER_SYSTEMD_DIR
+cp ./labtunnel@.service ./loop@.service $USER_SYSTEMD_DIR
 systemctl --user daemon-reload
 echo "Successfully installed systemd template labtunnel@.service in ~/.config/systemd/user."
 echo "Questions? Raise an issue at github.com/renxida/labtunnel"
